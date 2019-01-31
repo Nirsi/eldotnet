0799d5d4275e13309ad272543f39cf9bc9ea910f
Novák Jan <enrich.silen@gmail.com>
2019-01-31 16:32:55 +0100

    Cleanup and ToString for registers

--- Data/Registers.cs
+++ Data/Registers.cs
@@ -4,7 +4,13 @@ namespace eldotnet.Data
 {
     public struct Registers
     {
-        public static Register16 HL1 = new Register16();
-        public static Register16 HL2 = new Register16();
+        public static Register16 R1 = new Register16();
+        public static Register16 R2 = new Register16();
+        public static byte       C;
+
+        public static string[] ToArray()
+        {
+            return new string[] {"R1", "R2", "C"};
+        }
     }
 }
