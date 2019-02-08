00e6da132718a03d529a51cfc1bd36a0b57e48a9
Novák Jan <enrich.silen@gmail.com>
2019-02-08 08:40:05 +0100

    Corection for 16-bit alignment
    correction from using only 8-bit to 16-bit as name of Register suggest

--- Data/Register16.cs
+++ Data/Register16.cs
@@ -16,8 +16,8 @@ namespace eldotnet.Data
             set{
                 string[] vals = new string[2];
 
-                vals[0] = Convert.ToString(value, 2).PadLeft(8, '0').Substring(0, 4);
-                vals[1] = Convert.ToString(value, 2).PadLeft(8, '0').Substring(4, 4);
+                vals[0] = Convert.ToString(value, 2).PadLeft(16, '0').Substring(0, 8);
+                vals[1] = Convert.ToString(value, 2).PadLeft(16, '0').Substring(8, 8);
                
                 H = Convert.ToByte(vals[0], 2);
                 L = Convert.ToByte(vals[1], 2);
@@ -29,7 +29,7 @@ namespace eldotnet.Data
                 "4 lower bits: {3}\n"+
                 "4 higher bits converted: {4}\n"+
                 "4 lower bits converted: {5}",
-                 value, Convert.ToString(value, 2).PadLeft(8, '0'), vals[0], vals[1], this.H, this.L
+                 value, Convert.ToString(value, 2).PadLeft(16, '0'), vals[0], vals[1], this.H, this.L
                 ));
             }
          }
