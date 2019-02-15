0f1ed1f70d4d47c4726dac0545da301d19f179bb
Novák Jan <enrich.silen@gmail.com>
2019-02-15 16:21:36 +0100

    Base for instruction handling on runtime
    Base for instruction handling on runtime. For now loading source
    directly from Run methond.
    TODO: reading source file

--- Asm/Runtime.cs
+++ Asm/Runtime.cs
@@ -1,3 +1,4 @@
+using System;
 using eldotnet.Data;
 
 namespace eldotnet.Asm
@@ -11,20 +12,29 @@ namespace eldotnet.Asm
            Source = Parser.ReadSourceLines(sourcepath);
         }
 
-        public static void Run()
+        public static void Run(string source)
         {
-            foreach (string line in Source)
+            //foreach (string line in Source)
             {
-                string[] parts = line.Split(' ');
+
+                //Temporary placeholder for actual loaded source
+                string[] parts = source.Split(' ');
 
                 switch(parts[0].ToLower())
                 {
                     case "add":
                         if(Registers.IsRegister(parts[2]))
-                            Execution.Add<Register16>(Registers.NameToRegister(parts[1]), Registers.NameToRegister(parts[2]));
+                            Execution.Add<Register>(Registers.NameToRegister(parts[1]), Registers.NameToRegister(parts[2]));
                         else
-                            Execution.Add<short>(Registers.NameToRegister(parts[1]), 0);
-                        break; 
+                            Execution.Add<short>(Registers.NameToRegister(parts[1]), Convert.ToInt16(parts[2]));
+                    break; 
+                    
+                    case "mov":
+                        if(Registers.IsRegister(parts[2]))
+                            Execution.Mov<Register>(Registers.NameToRegister(parts[1]), Registers.NameToRegister(parts[2]));
+                        else
+                            Execution.Mov<short>(Registers.NameToRegister(parts[1]), Convert.ToInt16(parts[2]));
+                    break;
                 }
 
             }
