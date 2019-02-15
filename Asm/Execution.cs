f901b84809a62450dea63bed2f7e7f692377d781
Novák Jan <enrich.silen@gmail.com>
2019-02-15 15:10:04 +0100

    Mov instruction added
    Mov instruction by new design

--- Asm/Execution.cs
+++ Asm/Execution.cs
@@ -10,37 +10,42 @@ namespace eldotnet.Asm
         #region instruction handling
 
         protected static Type rType = typeof(Register16);
-        public static void Mov<T1>(Register16 register, T1 arg2)
+        public static void Mov<T1>(Register register, T1 arg2)
         {
-            
-            if(arg2.GetType() == rType)
+            if(register.GetType() == typeof(Register16))
             {
-                register.value = (arg2 as Register16).value;
-            }
-            else
-            {
-                register.value = Convert.ToInt16(arg2);
-            }
-            Registers.C.value += (short)1;
-            Log.Out.LogDebug(string.Format("Line count increased to {0}", Registers.C.value));
-        }
+                if(arg2.GetType() == typeof(Register16))
+                {
+                    (register as Register16).value = (arg2 as Register16).value;
+                }
+                else if(arg2.GetType() == typeof(Register8))
+                {
+                    (register as Register16).value = (arg2 as Register8).value; 
+                }
+                else
+                {
+                    (register as Register16).value = Convert.ToInt16(arg2);
+                }
 
-        /* 
-        public static void Add<T1>(Register16 register, T1 arg2)
-        {
-            if(arg2.GetType() == rType)
-            {
-                    register.value += (arg2 as Register16).value;
             }
             else
             {
-                register.value += Convert.ToInt16(arg2);
+                if(arg2.GetType() == typeof(Register16))
+                {
+                    (register as Register8).value = Convert.ToByte((arg2 as Register16).value);
+                }
+                else if(arg2.GetType() == typeof(Register8))
+                {
+                    (register as Register8).value = (arg2 as Register8).value; 
+                }
+                else
+                {
+                    (register as Register8).value = Convert.ToByte(arg2);
+                }
             }
             Registers.C.value += (short)1;
             Log.Out.LogDebug(string.Format("Line count increased to {0}", Registers.C.value));
         }
-        
-        */
 
         public static void Add<T1>(Register register, T1 arg2)
         {
