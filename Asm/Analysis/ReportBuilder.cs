b6593fe016de7fa3d3ede575290b9daa8e7f3abb
Novák Jan <enrich.silen@gmail.com>
2019-02-22 16:27:45 +0100

    Report minor edit and normalization of Program class

--- Asm/Analysis/ReportBuilder.cs
+++ Asm/Analysis/ReportBuilder.cs
@@ -11,11 +11,11 @@ namespace eldotnet.Asm.Analysis
             Log.Out.LogRuntime(string.Format(
                 "{0}\n"+
                 "----------------------------------------------\n"+
-                "{3}|\n"+
+                "{3} |\n"+
                 //source line position in report
-                "{1}|  {2}\n"+
-                "{3}|\n"+
-                "{3}|\n"+
+                "{1} |   {2}\n"+
+                "{3} |\n"+
+                "{3} |\n"+
                 "----------------------------------------------"
                 , errorTitle, lineNumber, ErrorMessage, padding
             ));
@@ -38,6 +38,7 @@ namespace eldotnet.Asm.Analysis
 
         internal static void ParamNotARegister(int lineNumber, string originalLine)
         {
-            GenerateReport(lineNumber, "Unknown register", originalLine);        }
+            GenerateReport(lineNumber, "Unknown register", originalLine);       
+        }
     }
 }
